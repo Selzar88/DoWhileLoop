8e7a3f45805f0ba03a969174a89fc3b6adadc4ab
wisni <wisni@LAPTOP-7AN5K9RK>
2022-06-04 14:02:31 +0100

    Add project files.

---
+++ DoWhileLoop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DoWhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int lenghtOfText = 0;
+            string wholeText = "";
+            do
+            {
+                Console.WriteLine("Please enter the name of a friend");
+                string nameOfAFriend = Console.ReadLine(); 
+                int currentLenght = nameOfAFriend.Length;
+                lenghtOfText += currentLenght;
+                wholeText += nameOfAFriend;
+            } while (lenghtOfText <20);
+            Console.WriteLine("Thanks, that was enough!");
+            Console.Read();
+        }
+    }
+}
